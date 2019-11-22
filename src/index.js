09dc875305275117f8c42df47bb2b2e7e20e5c9b
Manisha <manishas@answeriq.com>
2019-11-22 18:38:03 +0530

    comments describing component design

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import PowerBiWrapper from "./components/PowerBiWrapper";
 
 class PowerBiEmbed extends Component {
   render() {
+    // The variables names below can be modified by the user as per his convenience - they are just placeholders in the component props
+    // However the props names of PowerBiWrapper should not be modified as that's what goes into the Powerbi APIs
+
     var {
       AuthenticationType,
       accessToken,
@@ -17,13 +20,13 @@ class PowerBiEmbed extends Component {
       navContentPaneEnabled,
       width,
       groupId
-    } = this.props.config;
+    } = this.props.config;  // These variables are used by PowerBi APIs
 
     var {
       PbiFailErrorMessage, 
       invalidConfigErrorMessage,
       hideDefaultError
-    } = this.props;
+    } = this.props;   //These variables are for custom error handling
 
     return (
       <PowerBiWrapper
